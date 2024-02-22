cb285e41f413e0ca74c9cb630b7d7f800be7694c
DKrisztian1 <71670029+DKrisztian1@users.noreply.github.com>
2024-02-22 17:58:18 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -5,9 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ConsoleApp1;
+using ConsoleApp7;
 
-List<double> szamok= new List<double>();
+List<double> szamok = new List<double>();
 StreamReader sr = new StreamReader("szamok.txt");
 while (!sr.EndOfStream)
 {
@@ -16,15 +16,29 @@ while (!sr.EndOfStream)
 sr.Close();
 
 
+
+List<Telepules> Telepulesek = new List<Telepules>();
+StreamReader sr2 = new StreamReader("telepules.txt");
+while (!sr2.EndOfStream)
+{
+    string[] sor = sr2.ReadLine().Split(" ");
+    Telepulesek.Add(new Telepules(int.Parse(sor[0]), sor[1], double.Parse(sor[2].Replace('.', ',')), double.Parse(sor[3].Replace('.', ',')), double.Parse(sor[4].Replace('.', ',')), double.Parse(sor[5].Replace('.', ',')), sor[6], int.Parse(sor[7]), int.Parse(sor[8])));
+}
+sr2.Close();
+
+
+
+
+/*
 int hanyDarab = 0;
 
 foreach (var szam in szamok)
 {
-    if (szam % 3 != 0 && szam % 7 != 0 && szam % 9 != 0 && szam % 13 != 0 )
+    if (szam % 3 != 0 && szam % 7 != 0 && szam % 9 != 0 && szam % 13 != 0)
         hanyDarab++;
 }
 Console.WriteLine(hanyDarab);
-
+*/
 
 
 /*
@@ -50,10 +64,54 @@ Console.WriteLine(hanyDarab);
 
 
 
+/*
+//1.b
+
+List<char> MegadottSzamSzamjegyei = new List<char>();
+foreach (var jegy in "2354211341")
+    MegadottSzamSzamjegyei.Add(jegy);
+List<char> MasolatMegadottSzamSzamjegyei = new List<char>();
+
+int hanyAnaize = 0;
+
+List<char> JelenlegiSzamSzamjegyei = new List<char>();
+
+foreach (var szam in szamok)
+{
+    JelenlegiSzamSzamjegyei.Clear();
+    MasolatMegadottSzamSzamjegyei.Clear();
+    MasolatMegadottSzamSzamjegyei = MegadottSzamSzamjegyei;
+
+    foreach (char szamJegy in Convert.ToString(szam))
+    {
+        JelenlegiSzamSzamjegyei.Add(szamJegy);
+    }
+    
+
+
+    for (int i = 0; i < 9; i++)
+    {
+        if (MasolatMegadottSzamSzamjegyei.Contains(JelenlegiSzamSzamjegyei[i]))
+        {
+            JelenlegiSzamSzamjegyei.Remove(JelenlegiSzamSzamjegyei[i]);
+            MasolatMegadottSzamSzamjegyei.Remove(JelenlegiSzamSzamjegyei[i]);
+        }
+    }
+    
+
+    Console.Write(JelenlegiSzamSzamjegyei.Count() + ",");
+    Console.WriteLine(MegadottSzamSzamjegyei.Count());
+
+    
+   
+}
+Console.WriteLine(hanyAnaize);
+*/
 
 
 
 /*
+ //1.c
 Dictionary<int, int> SzamokElofordulasa = new Dictionary<int, int>();
 foreach (var szam in szamok)
 {
@@ -93,21 +151,15 @@ Console.WriteLine(RendezettSzamok.First().Key);
 
 
 
-1/c   válasz:21
-
+//1.c  Válasz:   21
 */
 
 
-/*
-List<Telepules> Telepulesek = new List<Telepules>();
-StreamReader sr2 = new StreamReader("telepules.txt");
-while (!sr2.EndOfStream)
-{
-    string[] sor = sr2.ReadLine().Split(" ");
-    Telepulesek.Add(new Telepules(int.Parse(sor[0]), sor[1], double.Parse(sor[2]), double.Parse(sor[3]), double.Parse(sor[4]), double.Parse(sor[5]), sor[6], int.Parse(sor[7]), int.Parse(sor[8]) ));
-}
-sr2.Close();
 
+
+
+/*
+//2.a
 Dictionary<string, double> MegyekLakossaga = new Dictionary<string, double>();
 foreach (var telepules in Telepulesek)
 {
@@ -118,5 +170,87 @@ foreach (var telepules in Telepulesek)
 }
 
 var LakossagAlapjanRendezett = MegyekLakossaga.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
-Console.WriteLine($"{LakossagAlapjanRendezett.First().Key} - { LakossagAlapjanRendezett.First().Value}");
+Console.WriteLine($"{LakossagAlapjanRendezett.ElementAt(1).Key} - { LakossagAlapjanRendezett.ElementAt(1).Value}");
+//2.a  Valasz:    PE - 1212055
+*/
+
+
+
+
+/*
+//2.b
+
+var SzelessegAlapjanRendezett = Telepulesek.OrderByDescending(x => x.Hosszusagi).ToList();
+Console.WriteLine(SzelessegAlapjanRendezett[0].TelepulesNeve);
+//2.b Valasz:  Uszka
+*/
+
+
+
+
+//2.c
+/*
+int hanyKecskemetSzegedKorzet = 0;
+foreach (Telepules telepules in Telepulesek)
+{
+    if(telepules.TavolsagKecskemettol <= 50 && telepules.TavolsagSzegedtol <= 50)
+        hanyKecskemetSzegedKorzet++;
+}
+Console.WriteLine(hanyKecskemetSzegedKorzet);
+//2.c Valasz: 24
+*/
+
+
+
+
+//2.e
+/*
+List<Telepules> BudaTelepulesek = new List<Telepules>();
+foreach (Telepules telepules in Telepulesek)
+{
+    if (telepules.TelepulesNeve.Contains("Buda") || telepules.TelepulesNeve.Contains("buda"))
+        BudaTelepulesek.Add(telepules);
+}
+
+var amiKell = BudaTelepulesek.OrderBy(x => x.Szelessegi).ToList().Take(3).Last();
+Console.WriteLine(amiKell.TelepulesNeve);
+//2.e Valasz: Budaors
+*/
+
+
+
+
+//2.f
+/*
+int amiIgen = 0;
+bool voltA, voltE, voltT;
+List<Telepules> AET = new List<Telepules>();
+
+void Reset()
+{
+    voltA = false; voltE = false; voltT = false;
+}
+
+foreach (Telepules telepules in Telepulesek)
+{
+    Reset();
+    foreach (char betu in telepules.TelepulesNeve)
+    {
+        if (betu == 'a' || betu == 'A')
+            voltA = true;
+
+        if ((betu == 't' || betu == 'T') && !voltE)
+            Reset();
+        else if ((betu == 'e' || betu == 'E') && voltA)
+            voltE = true;
+
+
+        else if ((betu == 't' || betu == 'T') && voltA && voltE)
+            voltT = true;
+    }
+    if (voltA && voltE && voltT)
+        amiIgen++;
+}
+Console.WriteLine(amiIgen);
+//2.f Valasz: 139
 */
